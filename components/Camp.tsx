303c7da1fda7bc13ebc439be6aa3d6cc69cf069f
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-01-09 09:15:15 -0300

    feat features section and rename campProps

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,7 +1,7 @@
 import { PEOPLE_URL } from '@/constants'
 import Image from 'next/image'
 
-interface CampProps {
+interface ICampSiteProps {
   backgroundImage: string
   title: string
   subtitle: string
@@ -13,7 +13,7 @@ function CampSite({
   title,
   subtitle,
   peopleJoined
-}: CampProps) {
+}: ICampSiteProps) {
   return (
     <div
       className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
